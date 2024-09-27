c2b07feef05ab1d09986942334e26132d158fe44
Johasit0 <johanmunoz319@gmail.com>
2024-09-27 13:58:25 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// Alquiler de Autos
+// Desarrollar un sistema básico de gestión de alquiler de autos que permita registrar clientes, autos disponibles y alquileres realizados. El sistema debe permitir:Registrar clientes con datos como nombre, correo electrónico y número de licencia.Registrar autos con información como marca, modelo, año y estado de disponibilidad.Realizar un alquiler registrando el cliente, el auto alquilado y la fecha de inicio y fin del alquiler.Mostrar un listado de autos disponibles para alquiler y un historial de alquileres.
+//
+
+require('dotenv').config()
