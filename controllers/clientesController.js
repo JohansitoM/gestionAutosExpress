c089ef464541e1b2437ec6b497f3e9a14b402416
Johasit0 <johanmunoz319@gmail.com>
2024-12-10 14:31:38 -0500

    conexion a neon

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -12,4 +12,17 @@ const crearCliente = async(req, res) => {
         res.status(500).json({error: e.message})
     }
 }
+
+const registrarCliente = async (req, res) => {
+    try {
+        const { nombre, correo, numeroLicencia, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const nuevoCliente = await Cliente.create({ nombre, correo, numeroLicencia, password: hashedPassword})
+
+        res.status
+    } catch (error) {
+        console.log("Error al registrar el cliente: ", error)
+        res.status(500).json({ error: "Error al registrar el cliente" })
+    }
+}
 module.exports = { crearCliente }
