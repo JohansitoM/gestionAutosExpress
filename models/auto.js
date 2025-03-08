598348a72c4a96b5635185491b023d980de0b059
Johansit0 <johanmunoz319@gmail.com>
2025-03-08 12:42:58 -0500

    otra vez

--- models/auto.js
+++ models/auto.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       marca: DataTypes.STRING,
       modelo: DataTypes.STRING,
       year: DataTypes.STRING,
-      estado: DataTypes.BOOLEAN,
+      estado: DataTypes.INTEGER,
       imageUrl: DataTypes.STRING,
       valorAlquiler: DataTypes.INTEGER,
     },
